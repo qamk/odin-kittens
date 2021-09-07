862a0404e2451f466f0ae6e4e26810975f2f0ce7
qamk <qamking@gmail.com>
2021-09-07 16:37:24 +0100

    Add initial Controller methods and routes

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,22 +1,43 @@
 class KittensController < ApplicationController
+  before_action :grab_kitten, except: %i[index new create]
+
+  # GET /kittens
   def index
+    @kittens = Kitten.all
   end
 
+  # GET /kittens/:id
   def show
   end
 
+  # GET /kittens/new
   def new
   end
 
+  # GET /kittens/:id/edit
   def edit
   end
 
+  # PUT or PATCH /kittens/:id
   def update
   end
 
+  # POST /kittens
   def create
+    @kitten = Kitten.new(kitten_params)
   end
 
+  # DELETE /kittens/:id
   def destroy
+  end
+
+  private
+
+  def grab_kitten
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  get 'kittens/index'
-  get 'kittens/show'
-  get 'kittens/new'
-  get 'kittens/edit'
-  get 'kittens/update'
-  get 'kittens/create'
-  get 'kittens/destroy'
+  root 'kittens#index'
+
+  resources :kittens
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
