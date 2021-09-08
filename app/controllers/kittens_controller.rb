c041af505cf3b054900905a1bbb2220ec03f9c3e
qamk <qamking@gmail.com>
2021-09-08 15:01:42 +0100

    Add validations; add #save branching logic in controller

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -21,15 +21,29 @@ class KittensController < ApplicationController
 
   # PUT or PATCH /kittens/:id
   def update
+    if @kitten.update(kitten_params)
+      redirect_to kitten_path(@kitten), notice: 'Your awwwh-some edit has been made'
+    else
+      flash.now[:error] = 'Your awwh-ful edit has not been made'
+      render :edit
+    end
   end
 
   # POST /kittens
   def create
     @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+      redirect_to kitten_path(@kitten), notice: 'That awwwh-dorable kitten has been added'
+    else
+      flash.now[:error] = 'That awwh-ful kitten has not been added'
+      render :new
+    end
   end
 
   # DELETE /kittens/:id
   def destroy
+    @kitten.destroy
+    redirect_to root_path, notice: 'Deleted that for you'
   end
 
   private
--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,2 +1,5 @@
 class Kitten < ApplicationRecord
+  validates :name, :age, :cuteness, presence: true
+  validates :age, :cuteness, numericality: { only_integer: true }
+  validates :cuteness, numericality: { in: 1..100 }
 end
